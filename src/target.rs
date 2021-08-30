e2be1e60520efa6a1e83b2073e6d47e1a4f1b84a
nickmachnik <nick.machnik@gmail.com>
2021-08-30 10:57:27 +0200

    small target trait

--- src/target.rs
+++ src/target.rs
@@ -1,1 +1,7 @@
-
+/// Distributions that can be targeted with the samplers in this crate
+pub trait Target<T> {
+    // Compute the logarithm of the target density function
+    fn log_density(position: T) -> f64;
+    // Compute the gradient logarithm of the target density function at a given position
+    fn log_density_gradient(position: T) -> f64;
+}
