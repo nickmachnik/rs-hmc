7598b7211309bfab18ace59ca8f7425aa2559761
nickmachnik <nick.machnik@gmail.com>
2022-04-05 13:16:09 +0200

    add approx and ndarray with blas support

--- src/lib.rs
+++ src/lib.rs
@@ -1,5 +1,8 @@
 //! Implementations of the standard Hamiltonian Monte Carlo and No U-Turn Sampler algorithms.
 
+extern crate blas_src;
+extern crate openblas_src;
+
 mod hmc;
 mod nuts;
 mod target;
